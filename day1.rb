89c2aca1200dc6824eed54e94a18bc2e053c311b
Rickard Olsson <rickard.olsson@hey.com>
2022-12-01 21:07:16 +0100

    Day1 part2

--- day1.rb
+++ day1.rb
@@ -2,7 +2,12 @@
 
 # This returns something like [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
 # Where each element in the array is an array of calorie numbers
-elf_inventories = (File.readlines 'day1_puzzle_input.txt').join.split("\n\n").map { |x| x.split("\n").map(&:to_i) }
-elf_with_most_calories = elf_inventories.map(&:sum).max
+elf_inventory_list = (File.readlines 'day1_puzzle_input.txt').join.split("\n\n").map { |x| x.split("\n").map(&:to_i) }
+elf_total_calories_list = elf_inventory_list.map(&:sum)
 
-puts "Part1: #{elf_with_most_calories}"
+
+elf_with_most_calories = elf_total_calories_list.max
+puts "Part 1: #{elf_with_most_calories}"
+
+top_three_elfs_calories = elf_total_calories_list.sort.reverse.take(3).sum
+puts "Part 2: #{top_three_elfs_calories}"
