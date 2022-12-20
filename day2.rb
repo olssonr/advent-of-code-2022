6f822fe11ec21f7ca2583cb470f04aa7cfdca481
Rickard Olsson <rickard.olsson@hey.com>
2022-12-20 22:42:19 +0100

    Day2 part2

--- day2.rb
+++ day2.rb
@@ -1,41 +1,60 @@
 # frozen_string_literal: true
-
-class StrategyGuide
-  attr_reader :rounds
-
-  def initialize(rounds)
-    @rounds = rounds
-  end
-
-  def total_score
-    rounds.sum(&:score)
-  end
-end
+require 'pry'
+require 'pry-byebug'
 
 class Round
-  def initialize(opponnents_shape, your_shape)
-    @opponnents_shape = opponnents_shape
+  def initialize(opponents_shape, your_shape, outcome: nil)
+    @opponents_shape = opponents_shape
     @your_shape = your_shape
+    @outcome = outcome
   end
 
   def outcome
-    if @opponnents_shape == @your_shape
+    @outcome || determine_outcome(@opponents_shape, @your_shape)
+  end
+
+  def determine_outcome(opponents_shape, your_shape)
+    if opponents_shape == your_shape
       Draw
-    elsif @opponnents_shape > @your_shape
+    elsif opponents_shape > your_shape
       Lose
     else
       Win
     end
   end
 
+  def your_shape
+    @your_shape || determine_your_shape_from_outcome
+  end
+
+  # TODO: Convenient to not have to define this logic again, but
+  # not very efficient to do this for all cases.
+  # Perhaps could build a map once and then just lookup?
+  def determine_your_shape_from_outcome
+    StrategyGuide::SHAPES.map(&:new).select { |shape| determine_outcome(@opponents_shape, shape) == @outcome }.first
+  end
+
   def score
-    outcome.score + @your_shape.score
+    outcome.score + your_shape.score
   end
 end
 
 class Outcome
   def self.score
     self::SCORE
+  end
+
+  def self.build_from(character)
+    case character
+    when "X"
+      Lose
+    when "Y"
+      Draw
+    when "Z"
+      Win
+    else
+      raise ArgumentError("Character not supported to build outcomes from")
+    end
   end
 end
 
@@ -125,13 +144,36 @@ class Scissors < Shape
   end
 end
 
+class StrategyGuide
+  attr_reader :rounds
+
+  SHAPES = [Rock, Paper, Scissors]
+
+  def initialize(rounds)
+    @rounds = rounds
+  end
+
+  def total_score
+    rounds.sum(&:score)
+  end
+end
+
 rounds = (File.readlines 'day2_puzzle_input.txt').map do |line| 
   opponents_character, your_character = line.split
   opponents_shape = Shape.build_from(opponents_character)
   your_shape = Shape.build_from(your_character)
   Round.new(opponents_shape, your_shape)
 end
+missunderstood_guide = StrategyGuide.new(rounds)
+
+puts "Part 1: #{missunderstood_guide.total_score}"
+
+rounds = (File.readlines 'day2_puzzle_input.txt').map do |line| 
+  opponents_character, outcome_character = line.split
+  opponents_shape = Shape.build_from(opponents_character)
+  wanted_outcome = Outcome.build_from(outcome_character)
+  Round.new(opponents_shape, nil, outcome: wanted_outcome)
+end
 guide = StrategyGuide.new(rounds)
 
-puts "Part 1: #{guide.total_score}"
-
+puts "Part 2: #{guide.total_score}"
